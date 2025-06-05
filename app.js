78a058d68cc931a91ad0130ac74f53b4d4aea048
Nguyen Thien An <nguyenthienan327@gmail.com>
2025-06-05 23:49:30 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -8,9 +10,24 @@ app.use(express.json());
 
 const contactsRouter = require("./app/routes/contact.route");
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Contact API");
-});
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  //Code o day se chay khi khong co route duoc dinh nghia nao
+  //khop voi yeu cau. Goi next() de chuyen sang middleware xu ly loi
+  return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls
+//Middleware xử lý lỗi tập trung
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application" });
+});
 
 module.exports = app;
